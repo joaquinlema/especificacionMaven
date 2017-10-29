f6ae78ed86cad557ab0521ff7e966d77350f0898
joaquin lema <joaco.lema@hotmail.com>
2017-10-29 02:20:10 -0300

    maven configuracion desde 0

---
+++ clase-maven/src/test/java/com/es/clase/maven/Testmio.java
@@ -0,0 +1,12 @@
+package com.es.clase.maven;
+
+import junit.framework.TestCase;
+
+public class Testmio extends TestCase {
+
+    public void testMetodo1()
+    {
+        assertTrue( true );
+    } 
+
+}
